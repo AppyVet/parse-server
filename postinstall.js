0033ffd852cfe4157bd5b5cd8030f5db1057c0d6
oli107 <oli@appyvet.com>
2020-08-10 16:28:34 +0100

    Update postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -4,36 +4,7 @@ const version = parseFloat(process.version.substr(1));
 const minimum = parseFloat(pkg.engines.node.match(/\d+/g).join('.'));
 
 module.exports = function () {
-  const openCollective = `
-                  1111111111
-               1111111111111111
-            1111111111111111111111
-          11111111111111111111111111
-        111111111111111       11111111
-       1111111111111             111111
-      1111111111111   111111111   111111
-      111111111111   11111111111   111111
-     1111111111111   11111111111   111111
-     1111111111111   1111111111    111111
-     1111111111111111111111111    1111111
-     11111111                    11111111
-      111111         1111111111111111111
-      11111   11111  111111111111111111
-       11111         11111111111111111
-        111111     111111111111111111
-          11111111111111111111111111
-            1111111111111111111111
-              111111111111111111
-                  11111111111
-
-
-        Thanks for installing parse 🙏
-  Please consider donating to our open collective
-      to help us maintain this package.
-
-  👉 https://opencollective.com/parse-server
-
-  `;
+  const openCollective = `Modified version from appyvet repo`;
   process.stdout.write(openCollective);
   if (version >= minimum) {
     process.exit(0);
